c8967a30ed2ab3547c9f9a402f9c0d0aa31c0310
Pedro Roque <roquefiesp@gmail.com>
2026-06-12 14:52:38 -0300

    Cria pagina PFormCliente com layout global

--- src/pages/PFormularios/PFormCliente/PFormCliente.tsx
+++ src/pages/PFormularios/PFormCliente/PFormCliente.tsx
@@ -1,15 +1,14 @@
 import type { JSX } from "react";
-import Navegacao from "../../../components/Navegacao/Navegacao";
+import Layout from "../../../components/Layout/Layout";
 import FormCliente from "../../../components/Formularios/FormCliente/FormCliente";
 
 function PFormCliente(): JSX.Element {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Navegacao paginaAtiva="novo-cliente" />
-      <main className="flex-1 py-8 px-4">
-        <FormCliente modo="criar" />
-      </main>
-    </div>
+    <Layout>
+      <div className="w-full min-h-full bg-slate-50">
+        <FormCliente />
+      </div>
+    </Layout>
   );
 }
 
