034d86a3bf02473958b02a7527771c1743fa90cd
Pedro Roque <roquefiesp@gmail.com>
2026-06-12 14:57:51 -0300

    Cria pagina PDetalhesEmprestimo com captura de id por parametro

---
+++ src/pages/PDetalhes/PDetalhesEmprestimo/PDetalhesEmprestimo.tsx
@@ -0,0 +1,19 @@
+import type { JSX } from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../../../components/Layout/Layout";
+import DetalhesEmprestimo from "../../../components/Detalhes/DetalhesEmprestimo/DetalhesEmprestimo";
+
+function PDetalhesEmprestimo(): JSX.Element {
+  const { id } = useParams<{ id: string }>();
+  const idEmprestimo = Number(id);
+
+  return (
+    <Layout>
+      <div className="w-full min-h-full bg-slate-50">
+        <DetalhesEmprestimo id_emprestimo={idEmprestimo} />
+      </div>
+    </Layout>
+  );
+}
+
+export default PDetalhesEmprestimo;
