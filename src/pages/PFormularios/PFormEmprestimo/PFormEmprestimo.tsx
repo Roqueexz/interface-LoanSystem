41f06504246c1eba0beffd3819efb57e35a05414
Pedro Roque <roquefiesp@gmail.com>
2026-06-12 14:54:47 -0300

    Cria pagina PFormEmprestimo com layout global

--- src/pages/PFormularios/PFormEmprestimo/PFormEmprestimo.tsx
+++ src/pages/PFormularios/PFormEmprestimo/PFormEmprestimo.tsx
@@ -1,15 +1,14 @@
 import type { JSX } from "react";
-import Navegacao from "../../../components/Navegacao/Navegacao";
+import Layout from "../../../components/Layout/Layout";
 import FormEmprestimo from "../../../components/Formularios/FormEmprestimo/FormEmprestimo";
 
 function PFormEmprestimo(): JSX.Element {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Navegacao paginaAtiva="novo-emprestimo" />
-      <main className="flex-1 py-8 px-4">
-        <FormEmprestimo modo="criar" clientesDisponiveis={[]} />
-      </main>
-    </div>
+    <Layout>
+      <div className="w-full min-h-full bg-slate-50">
+        <FormEmprestimo />
+      </div>
+    </Layout>
   );
 }
 
