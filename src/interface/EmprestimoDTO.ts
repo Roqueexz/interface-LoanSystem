b104edeb7f522a7971f3d1eeacfd2c09aeccea78
Pedro Roque <roquefiesp@gmail.com>
2026-07-03 11:55:45 -0300

    refactor(interface): padroniza interface de empréstimos para cadastro e edição

--- src/interface/EmprestimoDTO.ts
+++ src/interface/EmprestimoDTO.ts
@@ -1,13 +1,15 @@
 export default interface EmprestimoDTO {
-    id_emprestimo?: number;
-    id_cliente: number;
-    valor_emprestimo: number;
-    num_parcelas: number;
-    valor_parcela: number;
-    tipo_juros: string;
-    juros: number;
-    data_emprestimo: Date;
-    data_devolucao?: Date;
-    status_emprestimo?: boolean;
-    forma_pagamento?: string;
-}
+  id_emprestimo?: number;
+  id_cliente: number;
+  nome_cliente?: string;
+  sobrenome_cliente?: string;
+  valor_emprestimo: number;
+  num_parcelas: number;
+  valor_parcela: number;
+  tipo_juros: string;
+  juros: number;
+  data_emprestimo: Date | string;
+  data_devolucao?: Date | string;
+  forma_pagamento?: string | null;
+  status_emprestimo?: boolean;
+}
