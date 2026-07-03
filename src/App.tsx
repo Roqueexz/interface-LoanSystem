a290dc62f597a19580506c9e4962fa346caa9552
Pedro Roque <roquefiesp@gmail.com>
2026-07-03 15:26:44 -0300

    refactor(router): substitui PDetalhesCliente por PCliente como visão principal do cliente
    - remove rota duplicada de detalhes de cliente
    - centraliza visão do cliente em PCliente (dashboard)
    - simplifica arquitetura de navegação
    - melhora consistência entre listagem e visão individual

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import './App.css';
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import ProtectedRoute from './components/Rotas/ProtectedRoutes';
-import PLogin from './pages/Login/PLogin';
-import PHome from './pages/Inicio/PInicio';
-import PListagemCliente from './pages/Cliente/PListagemCliente/PListagemCliente';
-import PListagemEmprestimo from './pages/Emprestimo/PListagemEmprestimo/PListagemEmprestimo';
-import PCliente from './pages/Cliente/PCliente/PCliente';
-import PDetalhesEmprestimo from './pages/Emprestimo/PDetalhesEmprestimo/PDetalhesEmprestimo';
-import PEditarEmprestimo from './pages/Emprestimo/PEditarEmprestimo/PEditarEmprestimo';
+import ProtectedRoute from "./components/Rotas/ProtectedRoutes";
+
+import PLogin from "./pages/Login/PLogin";
+import PHome from "./pages/Inicio/PInicio";
+
+import PListagemCliente from "./pages/Cliente/PListagemCliente/PListagemCliente";
+import PCliente from "./pages/Cliente/PCliente/PCliente";
+
+import PListagemEmprestimo from "./pages/Emprestimo/PListagemEmprestimo/PListagemEmprestimo";
+import PDetalhesEmprestimo from "./pages/Emprestimo/PDetalhesEmprestimo/PDetalhesEmprestimo";
+import PEditarEmprestimo from "./pages/Emprestimo/PEditarEmprestimo/PEditarEmprestimo";
+
 import PEditarCliente from "./pages/Cliente/PEditarCliente/PEditarCliente";
-import PCaixa from './pages/Caixa/PCaixa';
+
+import PCaixa from "./pages/Caixa/PCaixa";
 
 function App() {
   const [isAuth, setIsAuth] = useState<boolean>(() => {
-    return localStorage.getItem('isAuth') === 'true';
+    return localStorage.getItem("isAuth") === "true";
   });
 
   const handleLoginSuccess = () => {
@@ -25,28 +30,58 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rota Raiz */}
+
+        {/* ROTA RAIZ */}
         <Route
           path="/"
           element={
-            isAuth
-              ? <ProtectedRoute isAuth={isAuth} element={PHome} />
-              : <PLogin onLoginSuccess={handleLoginSuccess} />
+            isAuth ? (
+              <ProtectedRoute isAuth={isAuth} element={PHome} />
+            ) : (
+              <PLogin onLoginSuccess={handleLoginSuccess} />
+            )
           }
         />
 
-        {/* Listagens (Todas Protegidas) */}
-        <Route path='/clientes' element={<ProtectedRoute isAuth={isAuth} element={PListagemCliente} />} />
-        <Route path='/emprestimos' element={<ProtectedRoute isAuth={isAuth} element={PListagemEmprestimo} />} />
-        <Route path='/caixa' element={<ProtectedRoute isAuth={isAuth} element={PCaixa} />} />
+        {/* LISTAGENS */}
+        <Route
+          path="/clientes"
+          element={<ProtectedRoute isAuth={isAuth} element={PListagemCliente} />}
+        />
 
-        {/* Formulários de Edição (Todos Protegidos) */}
-        <Route path='/editar-emprestimo/:id' element={<ProtectedRoute isAuth={isAuth} element={PEditarEmprestimo} />} />
-        <Route path="/editar-cliente/:id" element={<ProtectedRoute isAuth={isAuth} element={PEditarCliente} />} />
-  
-        {/* Detalhes de Visualização (Todos Protegidos) */}
-        <Route path='/clientes/:id' element={<ProtectedRoute isAuth={isAuth} element={PDetalhesCliente} />} />
-        <Route path='/emprestimos/:id' element={<ProtectedRoute isAuth={isAuth} element={PDetalhesEmprestimo} />} />
+        <Route
+          path="/emprestimos"
+          element={<ProtectedRoute isAuth={isAuth} element={PListagemEmprestimo} />}
+        />
+
+        <Route
+          path="/caixa"
+          element={<ProtectedRoute isAuth={isAuth} element={PCaixa} />}
+        />
+
+        {/* DETALHE / DASHBOARD DO CLIENTE (AGORA É O PCLIENTE) */}
+        <Route
+          path="/clientes/:id"
+          element={<ProtectedRoute isAuth={isAuth} element={PCliente} />}
+        />
+
+        {/* EDIÇÃO */}
+        <Route
+          path="/editar-cliente/:id"
+          element={<ProtectedRoute isAuth={isAuth} element={PEditarCliente} />}
+        />
+
+        <Route
+          path="/editar-emprestimo/:id"
+          element={<ProtectedRoute isAuth={isAuth} element={PEditarEmprestimo} />}
+        />
+
+        {/* DETALHE EMPRESTIMO */}
+        <Route
+          path="/emprestimos/:id"
+          element={<ProtectedRoute isAuth={isAuth} element={PDetalhesEmprestimo} />}
+        />
+
       </Routes>
     </BrowserRouter>
   );
