610a0164cc1c936f2073ab61ca1417c2ea93cda9
Pedro Roque <roquefiesp@gmail.com>
2026-07-04 09:59:39 -0300

    feat(routes): adiciona rotas para criação de cliente e empréstimo
    - /clientes/novo → PFormCliente
    - /emprestimos/novo → PFormEmprestimo

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ import PHome from "./pages/Inicio/PInicio";
 
 import PListagemCliente from "./pages/Cliente/PListagemCliente/PListagemCliente";
 import PCliente from "./pages/Cliente/PCliente/PCliente";
+import PEditarCliente from "./pages/Cliente/PEditarCliente/PEditarCliente";
+import PFormCliente from "./pages/Cliente/PFormCliente/PFormCliente"; // NOVO
 
 import PListagemEmprestimo from "./pages/Emprestimo/PListagemEmprestimo/PListagemEmprestimo";
 import PDetalhesEmprestimo from "./pages/Emprestimo/PDetalhesEmprestimo/PDetalhesEmprestimo";
 import PEditarEmprestimo from "./pages/Emprestimo/PEditarEmprestimo/PEditarEmprestimo";
-
-import PEditarCliente from "./pages/Cliente/PEditarCliente/PEditarCliente";
+import PFormEmprestimo from "./pages/Emprestimo/PFormEmprestimo/PFormEmprestimo"; // NOVO
 
 import PCaixa from "./pages/Caixa/PCaixa";
 
@@ -59,7 +60,18 @@ function App() {
           element={<ProtectedRoute isAuth={isAuth} element={PCaixa} />}
         />
 
-        {/* DETALHE / DASHBOARD DO CLIENTE (AGORA É O PCLIENTE) */}
+        {/* CRIAÇÃO - NOVAS ROTAS */}
+        <Route
+          path="/clientes/novo"
+          element={<ProtectedRoute isAuth={isAuth} element={PFormCliente} />}
+        />
+
+        <Route
+          path="/emprestimos/novo"
+          element={<ProtectedRoute isAuth={isAuth} element={PFormEmprestimo} />}
+        />
+
+        {/* DETALHE / DASHBOARD DO CLIENTE */}
         <Route
           path="/clientes/:id"
           element={<ProtectedRoute isAuth={isAuth} element={PCliente} />}
