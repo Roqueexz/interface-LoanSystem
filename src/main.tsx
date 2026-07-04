45180bf592227ecff31f8e93225d33d2f9c8dcef
Pedro Roque <roquefiesp@gmail.com>
2026-07-04 15:42:37 -0300

    feat(theme): configura Tailwind e CSS para suporte a dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import ToastProvider from './ui/Toast/ToastProvider.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <ThemeProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </ThemeProvider>
   </StrictMode>
 );
